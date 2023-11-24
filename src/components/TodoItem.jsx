ea86448aa9072848f11221d04f6024ce17134298
sardor_bek <143065285+S1ardor@users.noreply.github.com>
2023-11-25 03:17:17 +1000

    Add files via upload

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import React from 'react'
+
+function TodoItem(props) {
+  const {title, time, done, order, id, doneTodo, deleteTodo, editTodo} = props;
+  return (
+    <div className='d-flex justify-content-between align-items-center alert alert-primary'>
+      <div className="todo_time">
+      <h3>{order}. {title}</h3>
+      <p>{time}</p>
+      </div>
+      <div>
+
+      <button className="btn btn-primary me-3" onClick={ () => editTodo(id)}>Edit</button>
+
+        {done ? (
+          <button className="btn btn-danger" onClick={ () => deleteTodo(id)}>Delete</button>
+        ) : (
+          <button className="btn btn-success me-3" onClick={ () => doneTodo(id)}>Done</button>
+        )}
+        
+      </div>
+    </div>
+  )
+};
+TodoItem.propTypes = {
+  id: PropTypes.string,
+  title: PropTypes.string,
+  time: PropTypes.string,
+  done: PropTypes.bool,
+  order: PropTypes.number,
+  doneTodo: PropTypes.func,
+  deleteTodo: PropTypes.func,
+  editTodo: PropTypes.func,
+}
+
+export default TodoItem
